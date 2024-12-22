c5e7cf0cf2c05201855e2748e0b7a3d8495a4bfc
Matthew Miller <matthew@millerti.me>
2024-12-21 22:20:31 -0800

    Create a basic request options generator

---
+++ packages/server/src/index.test.ts
@@ -0,0 +1,62 @@
+import { assertEquals } from '@std/assert';
+import { stub } from '@std/testing/mock';
+
+import { generateRequestOptions } from './index.ts';
+import { _generateNonceInternals } from './crypto/generateNonce.ts';
+
+Deno.test('Should generate options', () => {
+  // Pretend globalThis.crypto doesn't exist
+  const mockGenerateNonce = stub(
+    _generateNonceInternals,
+    'stubThis',
+    () => '9kMlSgHQW8oBv_AdkSaZKM0ajrEUatzg2f24vV6AgnI',
+  );
+
+  const options = generateRequestOptions({
+    desiredClaims: ['familyName', 'givenName', 'ageOver21'],
+    requestOrigin: 'https://digital-credentials.dev',
+  });
+
+  assertEquals(
+    options,
+    {
+      digital: {
+        requests: [
+          {
+            response_type: 'vp_token',
+            response_mode: 'dc_api',
+            client_id: 'web-origin:https://digital-credentials.dev',
+            nonce: '9kMlSgHQW8oBv_AdkSaZKM0ajrEUatzg2f24vV6AgnI',
+            dcql_query: {
+              credentials: [
+                {
+                  id: 'cred1',
+                  format: 'mso_mdoc',
+                  meta: {
+                    doctype_value: 'org.iso.18013.5.1.mDL',
+                  },
+                  claims: [
+                    {
+                      namespace: 'org.iso.18013.5.1',
+                      claim_name: 'family_name',
+                    },
+                    {
+                      namespace: 'org.iso.18013.5.1',
+                      claim_name: 'given_name',
+                    },
+                    {
+                      namespace: 'org.iso.18013.5.1',
+                      claim_name: 'age_over_21',
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  );
+
+  mockGenerateNonce.restore();
+});
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,3 +1,61 @@
-export function helloSimpleDigiCreds() {
-  console.log("Hello from @simpledigicreds/server!");
+import { generateNonce } from './crypto/index.ts';
+
+/**
+ * - Tieing together DC API (browser), OID4VP (protocol), and mdoc (document format)
+ * - Unsigned requests only for now
+ */
+type ClaimName = 'familyName' | 'givenName' | 'ageOver21';
+
+/**
+ * Generate credential presentation request options suitable for passing into
+ * `navigator.credentials.get()` as per the Digital Credentials API. Only supports OID4VP + mdoc
+ * right now.
+ */
+export function generateRequestOptions(
+  { desiredClaims, requestOrigin }: { desiredClaims: ClaimName[]; requestOrigin: string },
+) {
+  return {
+    digital: {
+      requests: [
+        {
+          response_type: 'vp_token',
+          response_mode: 'dc_api',
+          client_id: `web-origin:${requestOrigin}`,
+          nonce: generateNonce(),
+          // https://openid.github.io/OpenID4VP/openid-4-verifiable-presentations-wg-draft.html#dcql_query
+          dcql_query: {
+            credentials: [
+              {
+                id: 'cred1',
+                format: 'mso_mdoc',
+                meta: {
+                  doctype_value: 'org.iso.18013.5.1.mDL',
+                },
+                claims: desiredClaims.map((claimName) => ({
+                  namespace: 'org.iso.18013.5.1',
+                  claim_name: toMdocClaimName(claimName),
+                })),
+              },
+            ],
+          },
+        },
+      ],
+    },
+  };
+}
+
+function toMdocClaimName(claimName: ClaimName) {
+  if (claimName === 'familyName') {
+    return 'family_name';
+  }
+
+  if (claimName === 'givenName') {
+    return 'given_name';
+  }
+
+  if (claimName === 'ageOver21') {
+    return 'age_over_21';
+  }
+
+  throw new Error(`Unrecognized ClaimName "${claimName}"`);
 }
