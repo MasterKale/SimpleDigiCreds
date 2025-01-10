c910fb9b57f50102f4a581565e9e7c2b01b91275
Matthew Miller <matthew@millerti.me>
2025-01-10 09:44:06 -0800

    Rearrange API method and export more types

---
+++ packages/server/src/generateRequestOptions.ts
@@ -0,0 +1,84 @@
+import { generateNonce } from './crypto/index.ts';
+import type {
+  OID4VPClaimQuery,
+  OID4VPCredentialQueryMdoc,
+  OID4VPSupportedMdocClaimName,
+} from './oid4vp.ts';
+
+/**
+ * OID4VP-specific request parameters
+ *
+ * https://openid.github.io/OpenID4VP/openid-4-verifiable-presentations-wg-draft.html#appendix-A.2
+ */
+export type DCAPIRequestOID4VP = {
+  /** The value `"vp_token"` */
+  response_type: 'vp_token';
+  /** The value `"dc_api"` */
+  response_mode: 'dc_api';
+  /** Ex: `"web-origin:https://example.com"` */
+  client_id: string;
+  /** Base64URL-encoded random bytes to ensure uniqueness of the presentation */
+  nonce: string;
+  /** An array of credentials being requested */
+  dcql_query: {
+    credentials: (OID4VPClaimQuery | OID4VPCredentialQueryMdoc)[];
+  };
+};
+
+/**
+ * Options suitable for passing into `navigator.credentials.get()` in the browser to request the
+ * presentation of a verifiable credential via the Digital Credentials API
+ */
+export type DCAPIRequestOptions = {
+  digital: {
+    requests: DCAPIRequestOID4VP[];
+  };
+};
+
+/**
+ * Generate credential presentation request options suitable for passing into
+ * `navigator.credentials.get()` as per the Digital Credentials API.
+ *
+ * Supported Protocols:
+ * - OID4VP
+ *
+ * Supported Document Formats:
+ * - mdoc
+ */
+export function generateRequestOptions(
+  { desiredClaims, requestOrigin }: {
+    desiredClaims: OID4VPSupportedMdocClaimName[];
+    requestOrigin: string;
+  },
+): DCAPIRequestOptions {
+  const mdocCredentialRequest: OID4VPCredentialQueryMdoc = {
+    id: 'cred1',
+    format: 'mso_mdoc',
+    meta: {
+      doctype_value: 'org.iso.18013.5.1.mDL',
+    },
+    claims: desiredClaims.map((claimName) => ({
+      namespace: 'org.iso.18013.5.1',
+      claim_name: claimName,
+    })),
+  };
+
+  return {
+    digital: {
+      requests: [
+        {
+          response_type: 'vp_token',
+          response_mode: 'dc_api',
+          client_id: `web-origin:${requestOrigin}`,
+          nonce: generateNonce(),
+          // https://openid.github.io/OpenID4VP/openid-4-verifiable-presentations-wg-draft.html#dcql_query
+          dcql_query: {
+            credentials: [
+              mdocCredentialRequest,
+            ],
+          },
+        },
+      ],
+    },
+  };
+}
