3102524b6c6dd4143dcdff611d1d02cbe8a079d1
Matthew Miller <matthew@millerti.me>
2025-01-10 09:23:18 -0800

    Add lots of typing to library method

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,19 +1,66 @@
 import { generateNonce } from './crypto/index.ts';
+import type {
+  OID4VPClaimQuery,
+  OID4VPCredentialQueryMdoc,
+  OID4VPSupportedMdocClaimName,
+} from './oid4vp.ts';
 
 /**
- * - Tieing together DC API (browser), OID4VP (protocol), and mdoc (document format)
- * - Unsigned requests only for now
+ * OID4VP-specific request parameters
+ *
+ * https://openid.github.io/OpenID4VP/openid-4-verifiable-presentations-wg-draft.html#appendix-A.2
  */
-type ClaimName = 'familyName' | 'givenName' | 'ageOver21';
+export type DCAPIRequestOID4VP = {
+  /** The value `"vp_token"` */
+  response_type: 'vp_token';
+  /** The value `"dc_api"` */
+  response_mode: 'dc_api';
+  /** Ex: `"web-origin:https://example.com"` */
+  client_id: string;
+  /** Base64URL-encoded random bytes to ensure uniqueness of the presentation */
+  nonce: string;
+  /** An array of credentials being requested */
+  dcql_query: {
+    credentials: (OID4VPClaimQuery | OID4VPCredentialQueryMdoc)[];
+  };
+};
+
+/**
+ * Options suitable for passing into `navigator.credentials.get()` in the browser to request the
+ * presentation of a verifiable credential via the Digital Credentials API
+ */
+export type DCAPIRequestOptions = {
+  digital: {
+    requests: DCAPIRequestOID4VP[];
+  };
+};
 
 /**
  * Generate credential presentation request options suitable for passing into
  * `navigator.credentials.get()` as per the Digital Credentials API. Only supports OID4VP + mdoc
  * right now.
+ *
+ * - Tying together DC API (browser), OID4VP (protocol), and mdoc (document format)
+ * - Unsigned requests only for now
  */
 export function generateRequestOptions(
-  { desiredClaims, requestOrigin }: { desiredClaims: ClaimName[]; requestOrigin: string },
-) {
+  { desiredClaims, requestOrigin }: {
+    desiredClaims: OID4VPSupportedMdocClaimName[];
+    requestOrigin: string;
+  },
+): DCAPIRequestOptions {
+  const mdocCredentialRequest: OID4VPCredentialQueryMdoc = {
+    id: 'cred1',
+    format: 'mso_mdoc',
+    meta: {
+      doctype_value: 'org.iso.18013.5.1.mDL',
+    },
+    claims: desiredClaims.map((claimName) => ({
+      namespace: 'org.iso.18013.5.1',
+      claim_name: claimName,
+    })),
+  };
+
   return {
     digital: {
       requests: [
@@ -25,37 +72,11 @@ export function generateRequestOptions(
           // https://openid.github.io/OpenID4VP/openid-4-verifiable-presentations-wg-draft.html#dcql_query
           dcql_query: {
             credentials: [
-              {
-                id: 'cred1',
-                format: 'mso_mdoc',
-                meta: {
-                  doctype_value: 'org.iso.18013.5.1.mDL',
-                },
-                claims: desiredClaims.map((claimName) => ({
-                  namespace: 'org.iso.18013.5.1',
-                  claim_name: toMdocClaimName(claimName),
-                })),
-              },
+              mdocCredentialRequest,
             ],
           },
         },
       ],
     },
   };
-}
-
-function toMdocClaimName(claimName: ClaimName) {
-  if (claimName === 'familyName') {
-    return 'family_name';
-  }
-
-  if (claimName === 'givenName') {
-    return 'given_name';
-  }
-
-  if (claimName === 'ageOver21') {
-    return 'age_over_21';
-  }
-
-  throw new Error(`Unrecognized ClaimName "${claimName}"`);
 }
