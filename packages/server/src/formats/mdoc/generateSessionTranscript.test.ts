02c58768e3dfd015da15900d7ec8b1463a30e76a
Matthew Miller <matthew@millerti.me>
2025-02-27 07:54:37 -0800

    Update SessionTranscript for OID4VP Draft 24

--- packages/server/src/formats/mdoc/generateSessionTranscript.test.ts
+++ packages/server/src/formats/mdoc/generateSessionTranscript.test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from '@std/assert';
-import { encodeHex } from '@std/encoding';
+import { decodeHex } from '@std/encoding';
 
 import type { DCAPIRequestOptions } from '../../dcapi.ts';
 import { generateSessionTranscript } from './generateSessionTranscript.ts';
@@ -12,17 +12,16 @@ Deno.test('should generate OID4VP-specific session transcript', async () => {
           response_type: 'vp_token',
           response_mode: 'dc_api',
           client_id: 'web-origin:http://localhost:8000',
-          nonce: 'lsJ0WeQDAYetlVL22qc7c1sqPUr5eVPQVR2KKcyi0S4',
+          nonce: 'Y_Sl5cgcgTiw7XnikC24SCDvPEFb81gcOp3lrsdSwZ8',
           dcql_query: {
             credentials: [
               {
                 id: 'cred1',
                 format: 'mso_mdoc',
-                // @ts-ignore
-                meta: { doctype_value: 'com.fido.phone' },
+                meta: { doctype_value: 'org.iso.18013.5.1.mDL' },
                 claims: [
-                  // @ts-ignore
-                  { namespace: 'com.fido.phone.1', claim_name: 'phone_number' },
+                  { path: ['org.iso.18013.5.1', 'family_name'] },
+                  { path: ['org.iso.18013.5.1', 'given_name'] },
                 ],
               },
             ],
@@ -35,7 +34,14 @@ Deno.test('should generate OID4VP-specific session transcript', async () => {
   const sessionTranscript = await generateSessionTranscript(options.digital.requests[0]);
 
   assertEquals(
-    encodeHex(sessionTranscript),
-    'd818583a83f6f682734f4944345650444341504948616e646f766572582033d5ee243801d28fdc2e4dcaca2239e87ee09e2949f33110ef11178975977ed4',
+    sessionTranscript,
+    [
+      null,
+      null,
+      [
+        'OpenID4VPDCAPIHandover',
+        decodeHex('fcfc8e99167eb7cf45197b6da20ef77015c2d4419ef5d414202577a1d33d8a19'),
+      ],
+    ],
   );
 });
--- packages/server/src/formats/mdoc/generateSessionTranscript.ts
+++ packages/server/src/formats/mdoc/generateSessionTranscript.ts
@@ -4,39 +4,34 @@ import type { DCAPIRequestOID4VP } from '../../dcapi.ts';
 import type { DCAPIOID4VPSessionTranscript } from './types.ts';
 
 /**
- * See OID4VP for SessionTranscript composition: https://openid.net/specs/openid-4-verifiable-presentations-1_0-24.html#appendix-B.3.4
+ * See OID4VP for SessionTranscript composition:
+ * https://openid.net/specs/openid-4-verifiable-presentations-1_0-24.html#appendix-B.3.4
  */
-export async function generateSessionTranscript(request: DCAPIRequestOID4VP): Promise<Uint8Array> {
+export async function generateSessionTranscript(
+  request: DCAPIRequestOID4VP,
+): Promise<DCAPIOID4VPSessionTranscript> {
   type OpenID4VPDCAPIHandoverInfo = [
     origin: string,
     client_id: string,
     nonce: string,
   ];
 
-  // Intentionally out of order as per CMWallet; `OpenID4VPDCAPIHandoverInfo` type is "correct"
   const handoverInfo: OpenID4VPDCAPIHandoverInfo = [
+    'http://localhost:8000',
     request.client_id,
     request.nonce,
-    'http://localhost:8000',
   ];
-  const handoverInfoCBOR = encodeCBOR(handoverInfo);
-  // Encoded CBOR Data Item (Tag No. 24) https://www.rfc-editor.org/rfc/rfc8949.html#embedded-di
-  const handoverDataCBORBstr = encodeCBOR(new CBORTag(24, handoverInfoCBOR));
-  const handoverInfoHash = await crypto.subtle.digest('SHA-256', handoverDataCBORBstr);
 
-  // console.log('handover', encodeHex(handoverInfoHash));
+  const handoverInfoCBOR = encodeCBOR(handoverInfo);
+  const handoverInfoHash = await crypto.subtle.digest('SHA-256', handoverInfoCBOR);
 
   const sessionTranscript: DCAPIOID4VPSessionTranscript = [
     null,
     null,
     [
-      // @ts-ignore: CMWallet uses "OID4VPDCAPIHandover" here
-      'OID4VPDCAPIHandover',
+      'OpenID4VPDCAPIHandover',
       new Uint8Array(handoverInfoHash),
     ],
   ];
-  const sessionTranscriptCBOR = encodeCBOR(sessionTranscript);
-  const sessionTranscriptCBORBstr = encodeCBOR(new CBORTag(24, sessionTranscriptCBOR));
-
-  return sessionTranscriptCBORBstr;
+  return sessionTranscript;
 }
