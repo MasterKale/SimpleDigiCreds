4beeb9ef6ef977f40f127f600d45147d2e4ce127
Matthew Miller <matthew@millerti.me>
2025-02-28 19:04:08 -0800

    Bring over and simplify convertCertBufferToPEM

---
+++ packages/server/src/helpers/convertCertBufferToPEM.test.ts
@@ -0,0 +1,24 @@
+import { assert, assertEquals } from '@std/assert';
+
+import { convertCertBufferToPEM } from './convertCertBufferToPEM.ts';
+
+Deno.test('should return pem when input is buffer', () => {
+  const input = new Uint8Array(128).fill(0);
+  const actual = convertCertBufferToPEM(input);
+  const actualPemArr = actual.split('\n');
+  assertEquals(
+    actual,
+    `-----BEGIN CERTIFICATE-----
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=
+-----END CERTIFICATE-----
+`,
+  );
+
+  assertEquals(actualPemArr[0], '-----BEGIN CERTIFICATE-----');
+  assert(actualPemArr[1].length <= 64);
+  assert(actualPemArr[2].length <= 64);
+  assert(actualPemArr[3].length <= 64);
+  assertEquals(actualPemArr[4], '-----END CERTIFICATE-----');
+});
---
+++ packages/server/src/helpers/convertCertBufferToPEM.ts
@@ -0,0 +1,20 @@
+import * as base64 from '@hexagon/base64';
+
+/**
+ * Convert buffer to an OpenSSL-compatible PEM text format.
+ */
+export function convertCertBufferToPEM(certBuffer: Uint8Array): string {
+  // We specifically do not want base64url used here, PEM is base64
+  const b64cert = base64.fromArrayBuffer(certBuffer, false);
+
+  let PEMKey = '';
+  for (let i = 0; i < Math.ceil(b64cert.length / 64); i += 1) {
+    const start = 64 * i;
+
+    PEMKey += `${b64cert.substr(start, 64)}\n`;
+  }
+
+  PEMKey = `-----BEGIN CERTIFICATE-----\n${PEMKey}-----END CERTIFICATE-----\n`;
+
+  return PEMKey;
+}
