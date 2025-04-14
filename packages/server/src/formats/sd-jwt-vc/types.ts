71474ad38607f4697001a13868865bf65fcc69c6
Matthew Miller <matthew@millerti.me>
2025-04-13 20:42:35 -0700

    Define some initial types

---
+++ packages/server/src/formats/sd-jwt-vc/types.ts
@@ -0,0 +1,59 @@
+type SDJWTHeader = {
+  alg: 'ES256';
+  typ: 'dc+sd-jwt';
+  x5c?: string[];
+};
+
+type SDJWTPayload = {
+  _sd: string[];
+  // https://www.ietf.org/archive/id/draft-ietf-oauth-selective-disclosure-jwt-17.html#hash_function_claim
+  _sd_alg?: SelectiveDisclosureAlgorithm;
+};
+
+/** https://www.ietf.org/archive/id/draft-ietf-oauth-selective-disclosure-jwt-17.html#name-issuer-signed-jwt */
+type IssuerSignedJWT = [
+  SDJWTHeader,
+  SDJWTPayload,
+  // TODO: SDJWTSignature
+];
+
+// One of https://www.iana.org/assignments/named-information/named-information.xhtml
+export type IANANamedInformationHashAlgorithm = 'sha-256' | 'sha-384' | 'sha-512';
+export type SelectiveDisclosureAlgorithm = IANANamedInformationHashAlgorithm;
+
+/**
+ * 4.2.1. Disclosures for Object Properties
+ * https://www.ietf.org/archive/id/draft-ietf-oauth-selective-disclosure-jwt-17.html#section-4.2.1
+ */
+export type DisclosureObjectProperty = [
+  salt: string,
+  claimName: string,
+  claimValue: unknown,
+];
+
+/**
+ * 4.2.2. Disclosures for Array Elements
+ * https://www.ietf.org/archive/id/draft-ietf-oauth-selective-disclosure-jwt-17.html#section-4.2.2
+ */
+export type DisclosureArrayElement = [
+  salt: string,
+  arrayElement: unknown,
+];
+
+/**
+ * Help understand when a parsed disclosure is an Object Property
+ */
+export function isDisclosureObjectProperty(
+  disclosure: DisclosureObjectProperty | DisclosureArrayElement,
+): disclosure is DisclosureObjectProperty {
+  return Array.isArray(disclosure) && disclosure.length === 3;
+}
+
+/**
+ * Help understand when a parsed disclosure is an Array Element
+ */
+export function isDisclosureArrayElement(
+  disclosure: DisclosureObjectProperty | DisclosureArrayElement,
+): disclosure is DisclosureArrayElement {
+  return Array.isArray(disclosure) && disclosure.length === 2;
+}
