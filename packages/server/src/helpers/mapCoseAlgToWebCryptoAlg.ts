d7ab30069c608a471ad6e8b3ed832c39709695de
Matthew Miller <matthew@millerti.me>
2025-04-16 22:23:32 -0700

    Replace some .indexOf() with .includes()

--- packages/server/src/helpers/mapCoseAlgToWebCryptoAlg.ts
+++ packages/server/src/helpers/mapCoseAlgToWebCryptoAlg.ts
@@ -5,15 +5,14 @@ import { COSEALG } from '../cose.ts';
  * Convert a COSE alg ID into a corresponding string value that WebCrypto APIs expect
  */
 export function mapCoseAlgToWebCryptoAlg(alg: COSEALG): SubtleCryptoAlg {
-  if ([COSEALG.RS1].indexOf(alg) >= 0) {
+  if ([COSEALG.RS1].includes(alg)) {
     return 'SHA-1';
-  } else if ([COSEALG.ES256, COSEALG.PS256, COSEALG.RS256].indexOf(alg) >= 0) {
+  } else if ([COSEALG.ES256, COSEALG.PS256, COSEALG.RS256].includes(alg)) {
     return 'SHA-256';
-  } else if ([COSEALG.ES384, COSEALG.PS384, COSEALG.RS384].indexOf(alg) >= 0) {
+  } else if ([COSEALG.ES384, COSEALG.PS384, COSEALG.RS384].includes(alg)) {
     return 'SHA-384';
   } else if (
-    [COSEALG.ES512, COSEALG.PS512, COSEALG.RS512, COSEALG.EdDSA].indexOf(alg) >=
-      0
+    [COSEALG.ES512, COSEALG.PS512, COSEALG.RS512, COSEALG.EdDSA].includes(alg)
   ) {
     return 'SHA-512';
   }
