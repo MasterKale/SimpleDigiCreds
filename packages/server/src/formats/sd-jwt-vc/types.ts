6be10419217442358cc3598bdd751424abfae3ae
Matthew Miller <matthew@millerti.me>
2025-04-16 22:22:23 -0700

    Remove some hand-rolled SD-JWT logic

--- packages/server/src/formats/sd-jwt-vc/types.ts
+++ packages/server/src/formats/sd-jwt-vc/types.ts
@@ -32,32 +32,6 @@ export type IssuerSignedJWTPayload = {
   };
 };
 
-/** https://www.ietf.org/archive/id/draft-ietf-oauth-selective-disclosure-jwt-17.html#name-issuer-signed-jwt */
-type IssuerSignedJWT = [
-  IssuerSignedJWTHeader,
-  IssuerSignedJWTPayload,
-  IssuerSignedJWTSignature: string,
-];
-
 // One of https://www.iana.org/assignments/named-information/named-information.xhtml
-type IANANamedInformationHashAlgorithm = 'sha-256' | 'sha-384' | 'sha-512';
+type IANANamedInformationHashAlgorithm = 'sha-256';
 export type SelectiveDisclosureAlgorithm = IANANamedInformationHashAlgorithm;
-
-/**
- * 4.2.1. Disclosures for Object Properties
- * https://www.ietf.org/archive/id/draft-ietf-oauth-selective-disclosure-jwt-17.html#section-4.2.1
- */
-export type DisclosureObjectProperty = [
-  salt: string,
-  claimName: string,
-  claimValue: unknown,
-];
-
-/**
- * 4.2.2. Disclosures for Array Elements
- * https://www.ietf.org/archive/id/draft-ietf-oauth-selective-disclosure-jwt-17.html#section-4.2.2
- */
-export type DisclosureArrayElement = [
-  salt: string,
-  arrayElement: unknown,
-];
