6b6ed91a12fc522282005f0375790c4801cf4062
Matthew Miller <matthew@millerti.me>
2025-04-17 22:44:41 -0700

    Add references for mdl options

--- packages/server/src/formats/mdoc/generateMDLRequestOptions.ts
+++ packages/server/src/formats/mdoc/generateMDLRequestOptions.ts
@@ -2,6 +2,10 @@ import type { OID4VPCredentialQueryMdoc } from '../../protocols/oid4vp.ts';
 
 /**
  * Generate an mDL-specific set of request options for the Digital Credentials API
+ *
+ * References:
+ * - https://openid.net/specs/openid-4-verifiable-presentations-1_0-24.html#name-mobile-documents-or-mdocs-i
+ * - https://www.iso.org/standard/69084.html
  */
 export function generateMDLRequestOptions({
   id,
