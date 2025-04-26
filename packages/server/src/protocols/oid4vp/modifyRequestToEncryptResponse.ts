f7359ddadce0a293cc9e404d9981ad35e4431e6e
Matthew Miller <matthew@millerti.me>
2025-04-25 21:25:53 -0700

    Add necessary OID4VP values for encryption

--- packages/server/src/protocols/oid4vp/modifyRequestToEncryptResponse.ts
+++ packages/server/src/protocols/oid4vp/modifyRequestToEncryptResponse.ts
@@ -1,10 +1,40 @@
-import { DigitalCredentialRequest } from '../../dcapi.ts';
+import type { DigitalCredentialRequest } from '../../dcapi.ts';
+import { generateEncryptionKeypair } from '../../helpers/generateEncryptionKeypair.ts';
 
 /**
  * Modify the request to ensure that the response is encrypted.
  */
-export function modifyRequestToEncryptResponse(
+export async function modifyRequestToEncryptResponse(
   request: DigitalCredentialRequest,
-): DigitalCredentialRequest {
-  return request;
+): Promise<{
+  request: DigitalCredentialRequest;
+  privateKeyJWK: JsonWebKey;
+}> {
+  const clientMetadata = request.data.client_metadata || {};
+
+  /**
+   * Change response_mode to "dc_api.jwt"
+   */
+  request.data.response_mode = 'dc_api.jwt';
+
+  /**
+   * Add `client_metadata.jwks`
+   */
+  const { privateKeyJWK, publicKeyJWK } = await generateEncryptionKeypair();
+  clientMetadata.jwks = {
+    keys: [publicKeyJWK],
+  };
+
+  /**
+   * Add `authorization_encrypted_response_alg` and `authorization_encrypted_response_enc`
+   */
+  clientMetadata.authorization_encrypted_response_alg = 'ECDH-ES';
+  clientMetadata.authorization_encrypted_response_enc = 'A128GCM';
+
+  /**
+   * Commit the changes to client_metadata
+   */
+  request.data.client_metadata = clientMetadata;
+
+  return { request, privateKeyJWK };
 }
