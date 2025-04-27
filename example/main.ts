2b58c5c8c69c969188de61a3a856a5b8d9c96250
Matthew Miller <matthew@millerti.me>
2025-04-26 19:59:35 -0700

    Add more requests to example server

--- example/main.ts
+++ example/main.ts
@@ -4,6 +4,10 @@ import { serveStatic } from "hono/deno";
 import {
   GeneratedPresentationRequest,
   generatePresentationRequest,
+  type OID4VPMDLCredentialOptions,
+  type OID4VPMdocCredentialOptionsFull,
+  type OID4VPMdocCredentialOptionsSimple,
+  type OID4VPSDJWTCredentialOptions,
   verifyPresentationResponse,
 } from "../packages/server/src/index.ts";
 
@@ -15,57 +19,66 @@ app.use("/static/*", serveStatic({ root: "./" }));
 app.get("/", serveStatic({ path: "./static/index.html" }));
 
 app.get("/options", async (ctx) => {
-  const mdlRequest = await generatePresentationRequest({
-    credentialOptions: {
-      format: "mdl",
-      desiredClaims: ["family_name", "given_name"],
-    },
-    requestOrigin: "http://localhost:8000",
-    encryptResponse: false,
-  });
+  // Simple mDL request
+  const mdlRequest: OID4VPMDLCredentialOptions = {
+    format: "mdl",
+    desiredClaims: ["family_name", "given_name"],
+  };
 
-  const mdocPIDRequest = await generatePresentationRequest({
-    credentialOptions: {
-      format: "mdoc",
-      doctype: "eu.europa.ec.eudi.pid.1",
-      claimPathPrefix: "eu.europa.ec.eudi.pid.1",
-      desiredClaims: ["family_name", "given_name", "nationality"],
-    },
-    requestOrigin: "http://localhost:8000",
-    encryptResponse: false,
-  });
+  // Simple European PID mdoc request
+  const mdocPIDRequest: OID4VPMdocCredentialOptionsSimple = {
+    format: "mdoc",
+    doctype: "eu.europa.ec.eudi.pid.1",
+    claimPathPrefix: "eu.europa.ec.eudi.pid.1",
+    desiredClaims: ["family_name", "given_name", "nationality"],
+  };
 
-  const mdocEMVCORequest = await generatePresentationRequest({
-    credentialOptions: {
-      format: "mdoc",
-      doctype: "com.emvco.payment_card",
-      claimPathPrefix: "com.emvco.payment_card.1",
-      desiredClaims: ["card_number", "card_network", "expiry_year", "expiry_month"],
-    },
-    requestOrigin: "http://localhost:8000",
-  });
+  // Simple EMVCo payment card mdoc request
+  const mdocEMVCORequest: OID4VPMdocCredentialOptionsSimple = {
+    format: "mdoc",
+    doctype: "com.emvco.payment_card",
+    claimPathPrefix: "com.emvco.payment_card.1",
+    desiredClaims: ["card_number", "card_network", "expiry_year", "expiry_month"],
+  };
 
-  const sdjwtvcRequest = await generatePresentationRequest({
-    credentialOptions: {
-      format: "sd-jwt-vc",
-      desiredClaims: ["family_name", "given_name"],
-      acceptedVCTValues: ["urn:eu.europa.ec.eudi:pid:1", "urn:eudi:pid:1"],
-    },
-    requestOrigin: "http://localhost:8000",
-  });
+  // Simple mVRC mdoc request
+  const mdocMVRCRequest: OID4VPMdocCredentialOptionsSimple = {
+    format: "mdoc",
+    doctype: "org.iso.7367.1.mVRC",
+    claimPathPrefix: "org.iso.18013.5.1",
+    desiredClaims: ["registration_number", "date_of_registration", "vehicle_holder"],
+  };
 
+  // Simple European PID SD-JWT-VC request
+  const sdjwtvcRequest: OID4VPSDJWTCredentialOptions = {
+    format: "sd-jwt-vc",
+    desiredClaims: ["family_name", "given_name"],
+    acceptedVCTValues: ["urn:eu.europa.ec.eudi:pid:1", "urn:eudi:pid:1"],
+  };
+
+  const sdjwtvcRequestComplex: OID4VPSDJWTCredentialOptions = {
+    format: "sd-jwt-vc",
+    desiredClaims: [
+      "given_name",
+      "family_name",
+      ["age_equal_or_over", "18"],
+    ],
+    acceptedVCTValues: ["urn:eudi:pid:1"],
+  };
   /**
    * Toggle between these to test either format (until both can be included in one DC API call)
    */
-  // const _request = mdlRequest;
-  const _request = sdjwtvcRequest;
-  const { dcapiOptions } = _request;
+  const request = await generatePresentationRequest({
+    credentialOptions: sdjwtvcRequestComplex,
+    requestOrigin: "http://localhost:8000",
+    encryptResponse: true,
+  });
 
-  console.log(JSON.stringify(_request, null, 2));
+  console.log(JSON.stringify(request, null, 2));
 
-  currentRequest = _request;
+  currentRequest = request;
 
-  return ctx.json(dcapiOptions);
+  return ctx.json(request.dcapiOptions);
 });
 
 app.post("/verify", async (ctx) => {
