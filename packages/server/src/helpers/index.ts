661047d0eaae2f5693191912eab0957f3dcd4ffd
Matthew Miller <matthew@millerti.me>
2025-04-28 11:46:17 -0700

    Group JWK helpers

--- packages/server/src/helpers/index.ts
+++ packages/server/src/helpers/index.ts
@@ -1,7 +1,7 @@
 export * as base64url from './base64url.ts';
 export * as x509 from './x509/index.ts';
 export { generateNonce } from './nonce.ts';
-export { importKey } from './importKey.ts';
+export { importPublicKeyJWK } from './cryptoJWK.ts';
 export { isDCAPIResponse } from '../dcapi/isDCAPIResponse.ts';
 export { mapCoseAlgToWebCryptoAlg } from './mapCoseAlgToWebCryptoAlg.ts';
 export { SimpleDigiCredsError, type SimpleDigiCredsErrorCode } from './simpleDigiCredsError.ts';
