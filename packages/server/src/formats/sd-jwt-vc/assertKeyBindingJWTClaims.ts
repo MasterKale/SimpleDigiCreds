13dcef05b33b5e3aaf23a6adac13c8df4709585a
Matthew Miller <matthew@millerti.me>
2025-05-08 08:28:35 -0700

    Add a bit more tolerance for clock skew

--- packages/server/src/formats/sd-jwt-vc/assertKeyBindingJWTClaims.ts
+++ packages/server/src/formats/sd-jwt-vc/assertKeyBindingJWTClaims.ts
@@ -20,11 +20,11 @@ export function assertKeyBindingJWTClaims({
   const issuedAtDate = new Date(payload.iat * 1000);
   const currentDate = new Date();
   // Add 1 second to the current date to account for clock skew
-  const currentDatePlus1Second = new Date(currentDate.getTime() + 1000);
+  const currentDatePlusSkew = new Date(currentDate.getTime() + 1500);
 
-  if (issuedAtDate > currentDatePlus1Second) {
+  if (issuedAtDate > currentDatePlusSkew) {
     const iatISO = issuedAtDate.toISOString();
-    const currentISO = currentDatePlus1Second.toISOString();
+    const currentISO = currentDatePlusSkew.toISOString();
     throw new SimpleDigiCredsError({
       message: `Key Binding JWT was issued at (${iatISO}), after the current date (${currentISO})`,
       code: 'SDJWTVerificationError',
